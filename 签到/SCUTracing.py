daec49387c89684c808a985d8a3c4ba1f20d6a56
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-07-02 22:41:34 +0800

    Update SCUTracing.py

--- 签到/SCUTracing.py
+++ 签到/SCUTracing.py
@@ -5,7 +5,7 @@
 @File ：SCUTracing.py
 @IDE ：PyCharm
 @Motto：ABC(Always Be Coding)
-@@Version: V1.0
+@@Version: V1.01
 @Description: 
 """
 import requests
@@ -168,14 +168,13 @@ def main():
         "zzs11": {"name": "",
                   "password": ""},
     }
-    msg_content = ""
+    msg_content = "#### **车队论坛登录**\n\n-------\n"
     id = 0
     for key in account:
         right_com = SCUTracing(account[key]['name'], account[key]['password'])
         data_dict = right_com.check_in()
         id += 1
-        msg_content = "".join((msg_content, "#### **车队论坛登录**", "\n\n", "-------", "\n",
-                               "##### <font color=#87CEEB>**账号", str(id), "**</font>\n\n"))
+        msg_content = "".join((msg_content, "##### <font color=#87CEEB>**账号", str(id), "**</font>\n\n"))
         for key in data_dict:
             msg_content = "".join((msg_content, "<font color=#DA70D6>", key, "</font>：", data_dict[key], "\n\n"))
         msg_content += "----------\n"
