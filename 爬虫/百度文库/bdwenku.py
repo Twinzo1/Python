1cb47785fac47429314700877fac8d1fabe6acd6
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-07-10 00:02:10 +0800

    支持docx下载

--- 爬虫/百度文库/bdwenku.py
+++ 爬虫/百度文库/bdwenku.py
@@ -5,15 +5,17 @@
 @File ：bdwenku.py
 @IDE ：PyCharm
 @Motto：ABC(Always Be Coding)
-@Version: V1.0
+@Version: V2.0
 @Description: 百度文库爬取
 """
 
 import requests
 import json
-from pyquery import PyQuery as pq
 import re
-
+from pyquery import PyQuery as pq
+from docx import Document
+from docx.shared import Inches, Pt
+from docx.enum.text import WD_LINE_SPACING
 
 def str2dict(dict_str):
     """
@@ -22,49 +24,8 @@ def str2dict(dict_str):
     """
     json_data = json.dumps(dict_str)
     json_str = json.loads(json_data)
-    cookie = dict([item.split("=", 1) for item in json_str.split(";")])
-    return cookie
-
-
-def get_html():
-    """
-    获取网页html
-    :param url:
-    :param headers:
-    :param code:
-    :return:
-    """
-    wenku_url = bidu_url
-    headers = {"user-agent": "LogStatistic"}
-    if not cookie_str or len(cookie_str.strip()) == 0:
-        res = requests.get(wenku_url, headers=headers)
-    else:
-        cookie = str2dict(cookie_str)
-        res = requests.get(wenku_url, cookies=cookie, headers=headers)
-    res.encoding = res.apparent_encoding
-    doc = pq(res.text)
-    return doc
-
-
-def get_Title():
-    title = get_html()('title')
-    return title.text().replace(" - 百度文库", "")
-
-
-def get_page_url():
-    """
-    用于获取每一页的url
-    :return:
-    """
-    content = get_html()('script')
-    url_co_dict = {}
-    for url_info in content.items():
-        if "var pageData" in url_info.text():
-            url_li = re.findall(r"=(.+);.?window.pageData", url_info.text())
-            url_co_dict = json.loads(url_li[0])
-
-    url_dict_li = json.loads(url_co_dict['readerInfo2019']['htmlUrls'])['json']
-    return url_dict_li
+    ret_dict = dict([item.split("=", 1) for item in json_str.split(";")])
+    return ret_dict
 
 
 def format_dict_str(dict_str):
@@ -73,16 +34,16 @@ def format_dict_str(dict_str):
     :param dict_str:
     :return:
     """
-    fstSplit = re.split(':', dict_str)
+    first_split = re.split(':', dict_str)
     colon_str = []
-    for sp1 in fstSplit:
-        secSplit = re.split(",", sp1)
+    for sp1 in first_split:
+        second_split = re.split(",", sp1)
         comma_str = []
-        for sp2 in secSplit:
+        for sp2 in second_split:
             if sp2.count('"') == 2 or sp2.count('"') == 0:
                 comma_str.append(sp2)
                 continue
-            if sp2.count('"')-2 != sp2.count(r'\"'):
+            if sp2.count('"') - 2 != sp2.count(r'\"'):
                 splitBack = sp2.replace(r'"', r'\"')
                 if sp2.count('"') % 2 != 0:
                     # 替换最后一个\"
@@ -97,74 +58,133 @@ def format_dict_str(dict_str):
         colon_str.append(sp1)
     return ":".join(colon_str)
 
+class BDWenKu:
+    def __init__(self, bd_url, bd_cookie=""):
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/78.0.3904.116 Safari/537.36 "
+        }
+        self.cookie = str2dict(bd_cookie) if bd_cookie != "" else ""
+        self.url = bd_url
+        self.pq_html = self.get_html()
+        self.title = self.get_title()
 
-def get_page_content(page_url):
-    """
-    获取每一页的内容
-    :return:
-    """
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.116 Safari/537.36"
-    }
-    if not cookie_str or len(cookie_str.strip()) == 0:
-        res = requests.get(page_url, headers=headers)
-    else:
-        cookie = str2dict(cookie_str)
-        res = requests.get(page_url, cookies=cookie, headers=headers)
-    res.encoding = res.apparent_encoding
-    res_txt = res.text
-    res_cn = res_txt.encode('utf-8').decode('unicode_escape', 'ignore')
-    res_cn_li = re.findall(r"\((.+)\)", res_cn)
+    def get_html(self):
+        """
+        获取网页html
+        :param url:
+        :param headers:
+        :param code:
+        :return:
+        """
+        headers = {"user-agent": "LogStatistic"}
+        res = self.with_cookie(self.url, headers)
+        doc = pq(res.text)
+        return doc
 
-    res_cn_dict = json.loads(format_dict_str(res_cn_li[0]))
-    res_co_li = res_cn_dict['body']
-    res_li = []
+    def get_title(self):
+        title = self.pq_html('title')
+        return title.text().replace(" - 百度文库", "")
 
-    for co_dict in res_co_li:
-        res_li.append([co_dict['c'], co_dict['p']['y']])
-
-    res_co_li = []
-    
-    # 使用坐标定位
-    for i, val in enumerate(res_li):
-        if i == 0:
-            res_co_li.append(val[0])
+    def with_cookie(self, url, headers=""):
+        headers = self.headers if headers == "" else headers
+        if not self.cookie or len(self.cookie.strip()) == 0:
+            res = requests.get(url, headers=headers)
         else:
-            if str(val[1]) == str(res_li[i-1][1]):
-                res_co_li[-1] = str(res_co_li[-1]) + str(val[0])
+            res = requests.get(url, cookies=self.cookie, headers=headers)
+        res.encoding = res.apparent_encoding
+        return res
+
+    def get_page_url(self):
+        """
+        用于获取每一页的url
+        :return:
+        """
+        content = self.pq_html('script')
+        for url_info in content.items():
+            if "var pageData" in url_info.text():
+                url_list = re.findall(r"=(.+);.?window.pageData", url_info.text())
+                content_have_url_dict = json.loads(url_list[0])
+                break
+
+        url_dict_list = json.loads(content_have_url_dict['readerInfo']['htmlUrls'])['json']
+        return url_dict_list
+
+    def get_page_content(self, page_url):
+        """
+        获取每一页的内容
+        :return:
+        """
+        res = self.with_cookie(page_url)
+        # 乱码变中文
+        res_cn = res.text.encode('utf-8').decode('unicode_escape', 'ignore')
+        res_cn_list = re.findall(r"\((.+)\)", res_cn)
+
+        res_cn_dict = json.loads(format_dict_str(res_cn_list[0]))
+        res_content_list = res_cn_dict['body']
+        res_list = []
+
+        for co_dict in res_content_list:
+            res_list.append([co_dict['c'], co_dict['p']['y']])
+
+        res_content_list = []
+
+        for i, val in enumerate(res_list):
+            if i != 0 and str(val[1]) == str(res_list[i-1][1]):
+                res_content_list[-1] = str(res_content_list[-1]) + str(val[0])
             else:
-                res_co_li.append(val[0])
+                res_content_list.append(val[0])
 
-    return res_co_li
+        return res_content_list
 
-
-def main():
-    url_info_dict_li = get_page_url()
-    title = get_Title()
-    for url_info in url_info_dict_li:
-        print("开始下载第{}页数据" .format(url_info['pageIndex']))
-
-        page_content = get_page_content(url_info['pageLoadUrl'])
-
-        # 写入文本
-        if url_info == url_info_dict_li[0]:
-            with open("./" + title + ".txt", 'w+', encoding='utf-8') as f:
-                print("开始下载第{}页数据" .format(url_info['pageIndex']))
-                for content_li in page_content:
-                    f.write(str(content_li) + "\n")
-                f.close()
+    def download(self, doc_type="txt"):
+        if doc_type = "word":
+            self.download_in_txt()
         else:
-            with open("./" + title + ".txt", 'a+', encoding='utf-8') as f:
-                print("开始下载第{}页数据" .format(url_info['pageIndex']))
+            self.download_in_docx()
+            
+    def download_in_txt(self):
+        url_info_dict_list = self.get_page_url()
+        for url_info_dict in url_info_dict_list:
+            page_content = self.get_page_content(url_info_dict['pageLoadUrl'])
+
+            # 写入文本
+            print("开始下载第{}页数据".format(url_info_dict['pageIndex']))
+            if url_info_dict == url_info_dict_list[0]:
+                open_model = 'w+'
+            else:
+                open_model = 'a+'
+            with open("./" + self.title + ".txt", open_model, encoding='utf-8') as f:
                 for content_li in page_content:
                     f.write(str(content_li) + "\n")
-                f.close()
+            f.close()
 
+    def download_in_docx(self):
+        # 创建文档对象
+        document = Document()
+
+        # 设置文档标题，中文要用unicode字符串
+        # document.add_heading(self.title, 0)
+        url_info_dict_list = self.get_page_url()
+        for url_info_dict in url_info_dict_list:
+            page_content = self.get_page_content(url_info_dict['pageLoadUrl'])
+            print("开始下载第{}页数据".format(url_info_dict['pageIndex']))
+            for content_li in page_content:
+                # 添加无序列表
+                paragraph = document.add_paragraph(str(content_li))
+                paragraph_format = paragraph.paragraph_format
+                paragraph_format.space_before = Pt(0)
+                paragraph_format.space_after = Pt(0)
+                paragraph.paragraph_format.line_spacing_rule = WD_LINE_SPACING.AT_LEAST
+                paragraph_format.line_spacing = Pt(13)
+        document.save("./" + self.title + ".docx")
 
 if __name__ == '__main__':
     # 填写你的cookie，不填只能爬取部分
-    cookie_str = ""
-    # 百度文库url
+    # cookie_str = ""
+    # # 百度文库url
     bidu_url = "https://wenku.baidu.com/view/0959513c6fdb6f1aff00bed5b9f3f90f76c64dc8.html"
-    main()
+    # main()
+    bidu = BDWenKu(bidu_url)
+    bidu.download("word")
 
